5c42c1ab911111991ce2af1e55d4cac49e8770d8
Danielsyafi <daniel.hidayat@binus.ac.id>
2023-03-21 03:41:12 +0700

    project_game

---
+++ game_Kioku/resources/views/start.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_start.css">
+    <title>Start</title>
+</head>
+<body>
+<div class="background_4">
+<div class="background_3">
+<div class="background_0">
+<div class="background_1"> 
+<div class="background_2">      
+   <div class="start">
+    <h1>Kiokuツ</h1>
+    <h3>highest level 1</h3>
+    <button id="my-button">START</button>
+
+<script>
+  // Ambil elemen tombol
+  let myButton = document.getElementById("my-button");
+  
+  // Tambahkan event listener untuk menangani klik pada tombol
+  myButton.addEventListener("click", function() {
+    window.location.href = "http://127.0.0.1:8000/game";
+  });
+</script>
+   </div>
+</div>  
+<div class="img">
+    <img src="img/tanah2.png" alt="">
+</div>
+</div>
+</div>
+</div>
+</div>
+</body>
+</html>
---
+++ game_Kioku/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/about', function () {
+    return view('welcome');
+});
+
+Route::get('/game', function () {
+    return view('game');
+});
+
+Route::get('/start', function () {
+    return view('start');
+});
+Route::get('/game-over', function () {
+    return view('game-over');
+});
+
